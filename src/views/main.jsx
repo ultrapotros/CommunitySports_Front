acda7594690ed83d098e57ea940056719059ea43
santi <santimezquita@hotmail.es>
2022-05-31 22:26:34 +0200

    first view of google maps

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -1,6 +1,8 @@
 import { useSession } from "helpers/session/useSession";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Map from '../components/Map'
+import datas from '../assets/data/centros_deportivos.json'
 
 export const Main = () => {
   const navigate = useNavigate();
@@ -8,6 +10,7 @@ export const Main = () => {
   return (
     <div className="app">
       <h1>Make History</h1>
+      <Map data={datas}/>
       <button onClick={() => navigate("/template")}>templates</button>
       <button onClick={() => logout()}>logout</button>
     </div>
