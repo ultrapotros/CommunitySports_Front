6a9157579b8802fb8fc3c0baab5e5925878e9949
Miguel Cid <miguel.cid.paz@gmail.com>
2022-05-31 19:37:56 +0200

    preparados estilos

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Views _____________________________________
 import { Session } from "./views/Session";
 import { Main } from "./views/main";
-import { Template } from "./views/Template";
 import { Error } from "./views/Error";
-import { Redirect } from "components/Redirect";
 // Context ___________________________________
 import { UserContextProvider } from "./contexts/user";
 
@@ -13,12 +11,13 @@ function App() {
   return (
     <UserContextProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Session />} />
-          <Route path="/template" element={<Template />} />
-          <Route path="/*" element={<Error />} />
-        </Routes>
+        <main className="app--main">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Session />} />
+            <Route path="/*" element={<Error />} />
+          </Routes>
+        </main>
         {/* Protects routes from not loged users */}
         {/* <Redirect /> */}
       </BrowserRouter>
