8aea2646e56634714b47e6cdd1cdf7d302632240
Miguel Cid <miguel.cid.paz@gmail.com>
2022-05-31 20:05:04 +0200

    implementando traducciones para centrar textos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 export const Header = () => {
     return (
         <header className="header--main">
-            <h1 className="header--title">Sports</h1>
+            <h1 className="header--title">CommunitySports</h1>
             <nav className="header--nav">
                 <p className="header--nav-link">Deportes</p>
                 <p className="header--nav-link">Centros</p>
                 <p className="header--nav-link">Eventos</p>
+                <p className="header--nav-link">Login</p>
             </nav>
         </header>
     )
