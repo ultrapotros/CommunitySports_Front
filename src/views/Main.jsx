ec85ea181fa891ddab16177814b4cd4ec8a7d5d6
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-06-01 11:28:06 +0200

    Add session modal

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,15 +1,22 @@
+import Modal from "components/Modal";
+import { ModalSession } from "components/SessionModal";
 import { useSession } from "helpers/session/useSession";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Main = () => {
   const navigate = useNavigate();
   const { logout } = useSession();
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className="app">
       <h1>Make History</h1>
       <button onClick={() => navigate("/template")}>templates</button>
       <button onClick={() => logout()}>logout</button>
+      <button onClick={() => setOpenModal(true)}>OpenModal</button>
+      <Modal onOpen={openModal} setOnOpen={setOpenModal}>
+        <ModalSession />
+      </Modal>
     </div>
   );
 };
