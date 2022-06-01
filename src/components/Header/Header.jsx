f6defe6fe5aa028b034ec40af89ada85d115d973
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-01 19:30:13 +0200

    header y general

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,38 @@
 import { useTranslation } from "react-i18next"
 
-//Icons
-import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
-import SportsIcon from '@mui/icons-material/Sports';
-import SportsScoreIcon from '@mui/icons-material/SportsScore';
-import LoginIcon from '@mui/icons-material/Login';
 
 export const Header = () => {
     const [t, i18n] = useTranslation("global");
 
     const navLinks = [
-        { icon: SportsBasketballIcon, text: t("header.sports") },
-        { icon: SportsIcon, text: t("header.centers") },
-        { icon: SportsScoreIcon, text: t("header.events") },
-        { icon: LoginIcon, text: t("header.login") }
+        { text: t("header.sports") },
+        { text: t("header.centers") },
+        { text: t("header.events") },
+        { text: t("header.login") }
     ]
+
+    const changeLanguage = () => {
+        if (i18n.language === "es") {
+            i18n.changeLanguage("en")
+        } else { i18n.changeLanguage("es") }
+    }
 
     return (
         <header className="header--main">
             <h1 className="header--title">{t("header.title")}</h1>
-            <button className="header--button-language">{t("header.language.english")}</button>
+            <button className="header--button-language" onClick={() => changeLanguage()}>{t("header.language.english")}</button>
+
             <nav className="header--nav">
                 {navLinks.map((e, i) => {
                     return (
-                        <div key={i} className="header--nav-group">
-                            <e.icon className="header--icon" scale="small" />
+                        <div key={i} className={i === navLinks.length - 1 ? "header--nav-group header--nav-login" : "header--nav-group"}>
                             <p className="header--nav-link">{e.text}</p>
                         </div>
                     )
                 })}
             </nav>
+
+
         </header>
     )
 }
