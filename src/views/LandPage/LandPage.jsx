b163c03ee1572570ead37c3178b3e8dc1b07a8b2
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-03 19:31:11 +0200

    all merges y map en landPage

--- src/views/LandPage/LandPage.jsx
+++ src/views/LandPage/LandPage.jsx
@@ -1,10 +1,13 @@
+import Map from "components/Map";
 import React from "react";
+import datas from '../../assets/data/centros_deportivos.json'
 
 export const LandPage = () => {
+  const myhome = { lat: datas[0].Longitud, lng: datas[0].Latitud }
 
   return (
     <div className="landpage--main">
-      <p>Hey</p>
+      <Map data={datas} home={myhome} />
     </div>
   );
 };
