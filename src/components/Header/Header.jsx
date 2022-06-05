1e63491847cff419c383ae601ea5e3a80a769ff9
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-05 08:43:41 +0200

    header styles stick

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import { useTranslation } from "react-i18next"
-
+import { useState, useEffect, useRef } from "react";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 export const Header = () => {
+    const header = useRef(null);
     const [t, i18n] = useTranslation("global");
+    const [navbar, setnavbar] = useState(false)
+    const [viewLanguages, setViewLanguages] = useState(false)
 
     const navLinks = [
         { text: t("header.sports") },
@@ -10,14 +15,27 @@ export const Header = () => {
         { text: t("header.events") }
     ]
 
-    const changeLanguage = () => {
-        if (i18n.language === "es") {
-            i18n.changeLanguage("en")
-        } else { i18n.changeLanguage("es") }
+    const allLanguages = [
+        { text: t("header.language.english"), code: 'en' },
+        { text: t("header.language.spanish"), code: 'es' }
+    ]
+
+    const changeHeader = () => {
+        window.scrollY > 0 ? setnavbar(true) : setnavbar(false)
     }
 
+    const selectedLanguage = () => {
+        if (i18n.language === 'en') {
+            return t("header.language.english")
+        } else {
+            return t("header.language.spanish")
+        }
+    }
+
+    window.addEventListener('scroll', changeHeader)
+
     return (
-        <header className="header--main">
+        <header className={navbar ? "header--main header--sticky" : "header--main"} ref={header}>
             <div className="app--group-width">
                 <div className="header--display-group header--display-left">
                     <h1 className="header--title">{t("header.title")}</h1>
@@ -32,7 +50,21 @@ export const Header = () => {
                     </nav>
                 </div>
                 <div className="header--display-group header--display-right">
-                    <button className="header--button-language" onClick={() => changeLanguage()}>{t("header.language.english")}</button>
+                    <button className="header--button-language" onClick={() => setViewLanguages(!viewLanguages)}>
+                        <p className="header--text-button">{selectedLanguage()}</p>
+                        {viewLanguages ?
+                            <KeyboardArrowUpIcon className="header--icon" fontSize="small" />
+                            : <KeyboardArrowDownIcon className="header--icon" fontSize="small" />}
+
+                        {viewLanguages ?
+                            <section className="header--modal-languages">
+                                {allLanguages.map((e, i) => {
+                                    return <p className="header--text-button" key={i} onClick={() => i18n.changeLanguage(e.code)}>{e.text}</p>
+                                })}
+                            </section>
+                            : null}
+                    </button>
+
                     <button className="header--button-login">{t("header.login")}</button>
                 </div>
             </div>
