f3e6499d61d28ca6c1b05e9dff107e8c50dacff9
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-06 13:20:57 +0200

    header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,16 @@ import { useTranslation } from "react-i18next"
 import { useState, useEffect, useRef } from "react";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import LanguageIcon from '@mui/icons-material/Language';
 
 export const Header = () => {
     const header = useRef(null);
     const [t, i18n] = useTranslation("global");
     const [navbar, setnavbar] = useState(false)
     const [viewLanguages, setViewLanguages] = useState(false)
+    const [viewOptions, setViewOptions] = useState(false)
 
     const navLinks = [
         { text: t("header.sports") },
@@ -66,6 +70,34 @@ export const Header = () => {
                     </button>
 
                     <button className="header--button-login">{t("header.login")}</button>
+                    <button className="header--button" onClick={() => setViewOptions(!viewOptions)}><MenuIcon className="header--icon" />
+                    </button>
+                    {viewOptions ?
+                        <section className="header--menu-modal">
+                            {navLinks.map((e, i) => {
+                                return <p className="header--nav-link header--modal-link" key={i}>{e.text}</p>
+                            })}
+                            <p className="header--modal-login" >{t("header.login")}</p>
+                            <button className="header--button header--close" onClick={() => setViewOptions(!viewOptions)} >
+                                <CloseIcon className="header--icon header--modal-icon" />
+                            </button>
+                        </section>
+                        : null}
+                    {!viewOptions ?
+                        <button className="header--button header--language" onClick={() => setViewLanguages(!viewLanguages)}>
+                            <LanguageIcon className="header--icon" />
+                        </button>
+                        : null}
+                    {viewLanguages ?
+                        <section className="header--modal-responsive">
+                            {allLanguages.map((e, i) => {
+                                return <p className="header--text-button" key={i} onClick={() => i18n.changeLanguage(e.code)}>{e.text}</p>
+                            })}
+                            <button className="header--button header--close" onClick={() => setViewLanguages(!viewLanguages)} >
+                                <CloseIcon className="header--icon header--modal-icon" />
+                            </button>
+                        </section>
+                        : null}
                 </div>
             </div>
         </header>
