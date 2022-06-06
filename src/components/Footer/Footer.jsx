97b26c1ef82c180b1ba1c70b6f7bf8fb329cfb39
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-06 15:49:19 +0200

    footer structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,23 @@
 export const Footer = () => {
 
     return (
-        <footer>
-            FOOTER
+        <footer className="footer--main">
+            <div className="app--wrapper">
+                <div className="footer--submain">
+                    <div className="footer--links">
+                        bucle de links
+                    </div>
+                    <div className="footer--final">
+                        <div className="footer--brand">
+                            marca
+                        </div>
+                        <p className="footer--copyright">footercopy</p>
+                        <div className="footer--social">
+                            redes sociales
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
