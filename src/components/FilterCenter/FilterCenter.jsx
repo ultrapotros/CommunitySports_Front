7da0678e3cc6998a1b6067bc3372dc9ab8e3f54b
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-14 12:32:30 +0200

    base del filter

---
+++ src/components/FilterCenter/FilterCenter.jsx
@@ -0,0 +1,19 @@
+export const FilterCenter = () => {
+    return (
+        <section className="filter_center--exterior-main">
+            <div className="filter_center--control-links">
+                link1 - link2 - link3
+            </div>
+            <div className="filter_center--body">
+                <div className="filter_center--body-map">
+                    MAPA
+                </div>
+                <div className="filter_center--control-buttons">
+                    BUTON1
+                    BUTON2
+                </div>
+            </div>
+        </section>
+    )
+}
+
