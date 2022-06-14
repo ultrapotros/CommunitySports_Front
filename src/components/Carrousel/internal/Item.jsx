a19674aff77679767c2e5b402cf313459fb3737f
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-14 09:17:57 +0200

    carrousel para landPage

---
+++ src/components/Carrousel/internal/Item.jsx
@@ -0,0 +1,16 @@
+export const Item = (props) => {
+
+    return (
+        <div className="carrousel--item-main">
+            <section className="carrousel--item-body">
+                <img className="carrousel--img" src="https://cdn.hsnstore.com/blog/wp-content/uploads/2015/05/motivacion-deportiva.jpg" alt="foto" />
+                <div className="carrousel--interior">
+                    <h1>holo</h1>
+                    <div className="carrousel--body-text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis suscipit fuga, recusandae amet dolore sequi hic sunt. itaque placeat!</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
--- src/views/LandPage/LandPage.jsx
+++ src/views/LandPage/LandPage.jsx
@@ -1,13 +1,15 @@
+import { Carrousell } from "components/Carrousel/Carrousell";
 import Map from "components/Map/Map";
 import React from "react";
 import datas from '../../assets/data/instalaciones.json'
 
 export const LandPage = () => {
-  const myhome = { lat: datas[0].Longitud, lng: datas[0].Latitud }
+  //const myhome = { lat: datas[0].Longitud, lng: datas[0].Latitud }
 
   return (
-    <div className="landpage--main">
-      <Map data={datas} home={myhome} />
-    </div>
+    <section className="landpage--main">
+      <Carrousell />
+    </section>
+
   );
 };
